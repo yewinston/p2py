c7c872b76b31946b6cba85259386814bf58bbd17
yew <39639471+yewinston@users.noreply.github.com>
2020-11-24 02:53:43 -0800

    requirements.txt + small additions

--- src/client_handler.py
+++ src/client_handler.py
@@ -4,18 +4,21 @@ Handler class for taking in user's inputs.
 
 from client import *
 import file_handler
+import sys
 
 def parseCommandLine():
-    print("parse command line todo")
+    for arg in sys.argv:
+        print(arg)
 
 def main():
     cli = Client()
+    parseCommandLine()
     
     # scenario 1: send a message
     message = cli.createTorrentPayload("../files/sample.txt")
     cli.send("socket", message)
 
-    # scenario 2: receive a message
+    # # scenario 2: receive a message
     cli.handleServerResponse(message)
 
     
