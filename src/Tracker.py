6de71ce80670bbbd0986d1ccd1f62f8ed8287ff0
trtri2 <39638939+trtri2@users.noreply.github.com>
2020-12-06 16:25:00 -0800

    added test infrastructure

--- src/Tracker.py
+++ src/Tracker.py
@@ -160,4 +160,5 @@ async def main():
     async with server:
         await server.serve_forever()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ test/run_tests.py
@@ -0,0 +1,29 @@
+import pytest
+
+from src.client import *
+from src.tracker import *
+
+def test_createServerRequest():
+    # sample :
+    cli = Client('127.0.0.1', '8080')
+    payload = cli.createServerRequest(opc=10)
+    assert(payload)
+    print('todo')
+
+def test_createPeerRequest():
+    print('todo')
+
+def test_createPeerResponse():
+    print('todo')
+
+def test_setPieceBuffer():
+    print('todo')
+
+def test_createServerResponse():
+    print('todo')
+
+def test_createNewTorrent():
+    print('todo')
+    
+
+
